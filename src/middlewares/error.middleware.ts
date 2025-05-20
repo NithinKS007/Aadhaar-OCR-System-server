cf42bf7d3234e90757d1d84061ee67e63d47c738
NithinKS007 <nithinksndkm@gmail.com>
2025-05-20 12:42:53 +0530

    changes made in error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -13,7 +13,8 @@ import { NextFunction, Request, Response } from "express";
 export const errorMiddleware = (
   err: AppError,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ): void => {
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   const message = err.message || StatusMessages.INTERNAL_SERVER_ERROR;
@@ -25,4 +26,5 @@ export const errorMiddleware = (
     method: req.method,
   });
   sendResponse(res, statusCode, null, message);
+  next();
 };
--- src/utils/app.error.ts
+++ src/utils/app.error.ts
@@ -4,7 +4,6 @@
 */
 class AppError extends Error {
   statusCode: number;
-
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
